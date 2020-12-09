3b47206f68a2651fef17e2767804b29166bad600
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2020-12-09 21:55:28 +0530

    Update add_items_to_sell.dart

--- lib/app/pages/sell/add_items_to_sell.dart
+++ lib/app/pages/sell/add_items_to_sell.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class AddItemsToSell extends StatefulWidget{
   @override
@@ -11,10 +10,12 @@ class _AddItemsToSell extends State<AddItemsToSell> {
   var name;
   var qty;
   var price;
+  var type;
 
   final nameCon = new TextEditingController();
   final qtyCon = new TextEditingController();
   final priceCon = new TextEditingController();
+  final typeCon = new TextEditingController();
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -52,51 +53,183 @@ class _AddItemsToSell extends State<AddItemsToSell> {
           ),
         ]
       )
-    ),
+      ),
       appBar: AppBar(
         title: Text('Sell'),
         backgroundColor: Colors.teal[800],
         elevation: 2.0,
+        actions: <Widget>[
+          Container(
+            height: 15.0,
+            child: RaisedButton(
+              child: Text('All produce'),
+              elevation: 2.0,
+              color: Colors.teal,
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/allitems');
+              }
+            )
+          ) 
+        ],
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: nameCon,
-                decoration: InputDecoration(
-                  hintText: 'Enter the name of the item'
-                ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(30.0),
+              padding: const EdgeInsets.all(10.0),
+              decoration: myBoxDecoration(),
+              child: Text(
+                "I'm using Flutter and I'd like to add a border to a widget (in this case, a Text widget)I tried TextStyle and Text, but I didn't see how to add a border",
+                style: TextStyle(fontSize: 20.0),
               ),
-              TextField(
-                controller: qtyCon,
-                decoration: InputDecoration(
-                  hintText: 'Enter the quantity'
-                ),
-              ),
-              TextField(
-                controller: priceCon,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: 'Enter the price'
-                ),
-              ),
-              RaisedButton(onPressed: () {
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'TYPE  : ',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19.0),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      controller: typeCon,
+                      decoration: InputDecoration(
+                        hintText: 'Vegetables/Seeds/Saplings',
+                      ),
+                    ),
+                  ),
+                ]
+              )
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'NAME  : ',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19.0),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      controller: nameCon,
+                      decoration: InputDecoration(
+                        hintText: 'Enter the name of your item',
+                      ),
+                    ),
+                  ),
+                ]
+              )
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'QUANTITY  : ',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19.0),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      controller: qtyCon,
+                      decoration: InputDecoration(
+                        hintText: 'Enter the quantity(eg: 1kg/5gm/1 sapling)',
+                      ),
+                    ),
+                  ),
+                ]
+              )
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'PRICE  : ',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19.0),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      controller: priceCon,
+                      decoration: InputDecoration(
+                        hintText: 'Enter the price',
+                      ),
+                    ),
+                  ),
+                ]
+              )
+            ),
+            SizedBox(height: 20.0),
+            RaisedButton(
+              onPressed: () {
                 setState((){
                   name = nameCon.text;
                   qty = qtyCon.text;
                   price = priceCon.text;
+                  type = typeCon.text;
                 });
-                Navigator.pushReplacementNamed(context, '/allitems');
-              },
-                child: Text('Submit'),
+                showAlertDialog(context);
+                },
+              child: Text(
+                'Add item',
+                style: TextStyle(color: Colors.white),
               ),
-            ],
-          )
+              color: Colors.teal,
+            ),
+            
+          ],
         )
-      ),
+      )
     );
   }
+}
+
+BoxDecoration myBoxDecoration(){
+  return BoxDecoration(
+    border: Border.all(
+      color: Colors.teal,
+      width: 5.0,
+    ),
+    borderRadius: BorderRadius.all(
+      Radius.circular(15.0)
+    )
+  );
+}
+
+showAlertDialog(BuildContext context){
+  Widget continueButton = FlatButton(
+    child: Text("Continue"),
+    onPressed: () {
+      Navigator.pushReplacementNamed(context, '/allitems');
+    },
+  );
+  AlertDialog alert = AlertDialog(
+    title: Text("Yes!"),
+    content: Text("Produce successfully added"),
+    actions: [
+      continueButton,
+    ],
+  );
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    }
+  );
 }
