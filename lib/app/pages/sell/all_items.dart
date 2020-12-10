6f041051c0ad092d3213da2511cc07ff61fe8c84
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2020-12-10 11:36:12 +0530

    Starting description added

--- lib/app/pages/sell/all_items.dart
+++ lib/app/pages/sell/all_items.dart
@@ -9,30 +9,68 @@ class AllItems extends StatelessWidget{
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
-            child: Text('Drawer Header'),
+            child: Text('Hope you\'re having a good day!'),
             decoration: BoxDecoration(color: Colors.green),
           ),
           ListTile(
-            title: Text('Item 1'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, '/maincatalog');
-            },
-          ),
-          ListTile(
-            title: Text('Item 2'),
+            title: Text('Home'),
             onTap: () {},
           ),
           ListTile(
-            title: Text('Item 2'),
+            title: Text('Grow'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text('Buy'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/catalog');
+            },
+          ),
+          ListTile(
+            title: Text('Sell'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/additemstosell');
+            },
+          ),
+          ListTile(
+            title: Text('Profile'),
             onTap: () {},
           ),
         ]
       )
     ),
     appBar: AppBar(
-      title: Text('All Items'),
+      title: Text('All Produce'),
       backgroundColor: Colors.teal[800],
       elevation: 2.0,
+      actions: <Widget>[
+          RaisedButton(
+            child: Text('Add new items'),
+            elevation: 2.0,
+            color: Colors.teal,
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/additemstosell');
+            }
+          )
+        ],
+    ),
+    body: SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              'Here are all of your items to sell',
+              style: TextStyle(fontSize: 19.0),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(border: Border.all(color: Colors.teal, width: 2.0)),
+            padding: const EdgeInsets.all(25.0),
+          ),
+        ],
+      )
+      
     ),
   );
   }
