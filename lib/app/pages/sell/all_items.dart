4bcc70f3303f8cc9dacf82c3e9fd7e1fb2f91f57
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2021-01-04 15:39:07 +0530

    commit

--- lib/app/pages/sell/all_items.dart
+++ lib/app/pages/sell/all_items.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:urban_roof/app/common/drawer_element.dart';
+import 'package:urban_roof/app/common/common_widgets.dart';
 
 class AllItems extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    drawer: DrawerElement(),
+    drawer: builddrawerelement(context),
+    bottomNavigationBar: Container(
+              height: 55,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[300],
+                          blurRadius: 20,
+                          spreadRadius: 1)
+                    ]),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    buildContainerBottomNav(Icons.ballot, '', context, isSelected: true),
+                    buildContainerBottomNav(Icons.notifications_on_rounded, '', context),
+                    buildContainerBottomNav(Icons.home, 'home', context),
+                    buildContainerBottomNav(Icons.person, 'profile', context),
+                    buildContainerBottomNav(Icons.shopping_cart, 'maincategory', context),
+                  ],
+                ),
+              )),
     appBar: AppBar(
-      title: Text('All Produce'),
-      backgroundColor: Colors.teal[800],
+      title: Text('SELL'),
+      centerTitle: true,
+      backgroundColor: Colors.green,
       elevation: 2.0,
       actions: <Widget>[
-          RaisedButton(
-            child: Text('Add new items'),
-            elevation: 2.0,
-            color: Colors.teal,
+        SizedBox(
+          width:55.0,
+          child: FlatButton(
+            child: Icon(Icons.add, color: Colors.white,),
+            //elevation: 2.0,
+            color: Colors.green,
             onPressed: () {
-              Navigator.pushReplacementNamed(context, '/additemstosell');
+              Navigator.pushReplacementNamed(context, '/additems');
             }
           )
+        )
+          
         ],
     ),
     body: SingleChildScrollView(
@@ -26,19 +57,87 @@ class AllItems extends StatelessWidget{
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            alignment: Alignment.center,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.teal[900])
+            ),
             child: Text(
               'Here are all of your items to sell',
-              style: TextStyle(fontSize: 19.0),
               textAlign: TextAlign.center,
-            ),
-            decoration: BoxDecoration(border: Border.all(color: Colors.teal, width: 2.0)),
-            padding: const EdgeInsets.all(25.0),
+              style: TextStyle(fontSize: 20.0),
+            )
           ),
+          itemflatbutton('Brinjal', 'saplingbrinjal'),
+          itemflatbutton('Cauliflower', 'saplingcauliflower'),
+          itemflatbutton('Brinjal', 'vegetablebrinjal'),
+          //itemflatbutton('Cabbage', 'saplingecabbage'),
+          itemflatbutton('Tomato', 'saplingtomato'),
+          itemflatbutton('Spinach', 'vegetablespinach'),
         ],
       )
       
     ),
   );
   }
+}
+
+BoxDecoration myBoxDecoration(String image){
+  return BoxDecoration(
+    color: Colors.white,
+    image: DecorationImage(
+      image: AssetImage('assets/images/$image.jpg') ,
+      fit: BoxFit.cover,
+    ),
+    borderRadius: BorderRadius.all(
+      Radius.circular(5.0)
+    )
+  );
+}
+
+Widget itemflatbutton(String item, String image) {
+  return FlatButton(
+            onPressed: () {},
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 100.0,
+                  width: 100.0,
+                  child: DecoratedBox(
+                    decoration: myBoxDecoration(image)
+                  ),
+                ),
+                SizedBox(width: 30.0),
+                Column(
+                  children: [
+                    Text(
+                      '$item',
+                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(height: 7.0),
+                    Text(
+                      'Rs. 50 per kg',
+                      style: TextStyle(fontSize: 17.0),
+                    ),
+
+                    Row(
+                      children: [
+                        Text(
+                          ' '
+                        )
+                      ],
+                    )
+                  ]
+                ),
+              ],
+            ),
+            shape: RoundedRectangleBorder(side: BorderSide(
+              color: Colors.grey,
+              width: 1.0,
+              style: BorderStyle.solid
+            ),
+          )
+          );
 }
