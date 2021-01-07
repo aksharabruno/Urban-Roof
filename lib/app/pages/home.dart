55f9705a0f308e394a40330ed783f271d982b016
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2021-01-08 03:31:45 +0530

    Update home.dart

--- lib/app/pages/home.dart
+++ lib/app/pages/home.dart
@@ -1,398 +1,175 @@
-//import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:urban_roof/app/common/common_widgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-import 'detailspage.dart';
+//import 'package:urban_roof/app/pages/home.dart';
 
-class HomePage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => new _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  String username = "";
+  String password = "";
+  //String final_response = "";
+  final _formkey = GlobalKey<FormState>();
+
+  final unCon = new TextEditingController();
+  final pwCon = new TextEditingController();
+
+  Future<void> _savingData() async {
+    final validation = _formkey.currentState.validate();
+    if(!validation){
+      return;
+    }
+    _formkey.currentState.save();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          drawer: builddrawerelement(context),
-          bottomNavigationBar: Container(
-              height: 55,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey[300],
-                          blurRadius: 20,
-                          spreadRadius: 1)
-                    ]),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    buildContainerBottomNav(Icons.ballot, 'allitems', context),
-                    buildContainerBottomNav(Icons.notifications_on_rounded, 'notification', context),
-                    buildContainerBottomNav(Icons.home, 'home', context, isSelected: true),
-                    buildContainerBottomNav(Icons.person, 'profile', context),
-                    buildContainerBottomNav(Icons.shopping_cart, 'maincategory', context),
-                  ],
-                ),
-              )),
-          appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.green[700],
-            elevation: 0,
-            leading: Builder(builder: (BuildContext context) {
-              return IconButton(
-                icon: Container(child: Icon(Icons.menu)),
-                onPressed: () {},
-              );
-            }),
-            title: Text(
-              "URBAN ROOF",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+    return new Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            //SizedBox(height:50.0),
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                    child: Text('Welcome to',
+                        style: TextStyle(
+                            fontSize: 50.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
+                    child: Text('Urban Roof',
+                        style: TextStyle(
+                            fontSize: 65.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green)),
+                  ),
+                ],
               ),
             ),
-            actions: <Widget>[
-              IconButton(
-                icon: Container(child: Icon(Icons.search)),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          body: Column(
-            children: [
-              SizedBox(
-                height: 8,
-              ),
-              Padding(padding: new EdgeInsets.all(5)),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Padding(padding: new EdgeInsets.all(2)),
-                  Text("Start your Farm",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 0.5,
-                      )),
-                ],
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  buildFlatButton("Blogs", context, 'home', isSelected: true),
-                  buildFlatButton("Grow", context, 'grow'),
-                  buildFlatButton("Reviews", context, 'home'),
-                ],
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius:
-                          BorderRadius.only(topLeft: Radius.circular(40)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey[350],
-                          spreadRadius: 1,
-                          blurRadius: 15,
-                        )
-                      ]),
-                  child: SingleChildScrollView(
-                    physics: BouncingScrollPhysics(),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            buildColumnAtTop("Top", isSelected: true),
-                            buildColumnAtTop("Popular"),
-                            buildColumnAtTop("Trending"),
-                            buildColumnAtTop("Editor's choice"),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        MyblogList(),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        LineBar(),
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(
-                            children: [
-                              Text(
-                                "Beginner Special",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                ),
-                              ),
-                              Spacer(),
-                              Text(
-                                "View All",
-                                style:
-                                    TextStyle(fontSize: 15, color: Colors.grey),
-                              )
-                            ],
-                          ),
-                        ),
-                        SingleChildScrollView(
-                          physics: BouncingScrollPhysics(),
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            children: [
-                              buildBottomContainer("4",
-                                  "Easy Chilli", "Author"),
-                              buildBottomContainer("5",
-                                  "Very green peas", "Author"),
-                              buildBottomContainer("6",
-                                  "Organic eggplants", "Author"),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        LineBar2(),
-                      ],
+            SizedBox(height:50.0),
+            Container(
+                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                child: Column(
+                  key: _formkey,
+                  children: <Widget>[
+                    TextField(
+                     // controller: unCon,
+                      decoration: InputDecoration(
+                          labelText: 'USERNAME',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Raleway',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green))),
+                      //onSaved: (value){username = value;},
                     ),
-                  ),
+                    SizedBox(height: 20.0),
+                    TextField(
+                      //controller: pwCon,
+                      decoration: InputDecoration(
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Raleway',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green))),
+                      obscureText: true,
+                      //onSaved: (value){password = value;},
+                    ),
+                    SizedBox(height: 5.0),
+                    Container(
+                      alignment: Alignment(1.0, 0.0),
+                      padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                      child: InkWell(
+                        child: Text(
+                          'Forgot Password',
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Raleway',
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    Container(
+                      height: 40.0,                      
+                      child: Material(
+                        borderRadius: BorderRadius.circular(50.0),
+                        shadowColor: Colors.greenAccent,                        
+                        elevation: 7.0,
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+                          color: Colors.green,
+                          onPressed: () async {
+                            //Navigator.of(context).pushNamed('/home');
+                            setState(() {
+                              username = unCon.text;
+                              password = pwCon.text;
+                            });
+                            
+                            /*_savingData();
+                            final url = 'http://10.0.2.15:5000/login';
+                            var data = {'username': username, 'password': password};
+                            final response = await http.post(url, body: json.encode(data));*/
+                            Navigator.of(context).pushNamed('/home');
+                            //Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(uname: username)));
+                            /*if(response.statusCode == 200){
+                                Navigator.of(context).pushNamed('/home');} else {
+                                  print('error');
+                                }*/
+                          },
+                          child: Center(
+                            child: Text(
+                              'LOG IN',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Raleway'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+              
+                  ],
+                )),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'New to Urban Roof ?',
+                  style: TextStyle(fontFamily: 'Raleway'),
                 ),
-              )
-            ],
-          ),
-        );
-  }
-
-
-
-  Container buildBottomContainer(String image, String text1, String text2) {
-    return Container(
-        height: 150,
-        width: 240,
-        margin: EdgeInsets.only(left: 20),
-        child: Row(
-          children: [
-            ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: SizedBox(
-                    height: 130,
-                    width: 100,
-                    child: Image.asset("assets/images/blog$image.jpg", fit: BoxFit.cover))),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    text1,
+                SizedBox(width: 5.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushNamed('/signup');
+                  },
+                  child: Text(
+                    'Register here',
                     style: TextStyle(
-                      fontSize: 15,
-                    ),
+                        color: Colors.green,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),
                   ),
-                  Spacer(),
-                  Text(
-                    text2,
-                    style: TextStyle(
-                      fontSize: 13,
-                      color: Colors.grey,
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             )
           ],
         ));
-  }
-
- 
-
-  Column buildColumnAtTop(String text, {bool isSelected = false}) {
-    return Column(
-      children: [
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: 18,
-            color: isSelected ? Colors.green[800] : Colors.grey,
-            letterSpacing: 0.5,
-          ),
-        ),
-        if (isSelected)
-          Container(
-              width: 5,
-              height: 5,
-              decoration: BoxDecoration(
-                color: Colors.green[800],
-                shape: BoxShape.circle,
-              ))
-      ],
-    );
-  }
-
-  FlatButton buildFlatButton(String text,  BuildContext context, String route, {bool isSelected = false}) {
-    return FlatButton(
-      onPressed: () {
-        Navigator.pushReplacementNamed(context, '/$route');
-      },
-      child: Text(
-        text,
-        style: TextStyle(
-          fontSize: 15,
-          color: isSelected ? Colors.white : Colors.black,
-          letterSpacing: 0.5,
-        ),
-      ),
-      shape: StadiumBorder(),
-      color: isSelected ? Colors.lightGreen : Colors.grey[300],
-    );
-  }
-}
-
-class MyblogList extends StatelessWidget {
-  const MyblogList({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          SizedBox(
-            width: 20,
-          ),
-          
-            buildColumnWithRow(
-                "1",
-                "Fresh from Home",context),
-          
-          buildColumnWithRow("2",
-              "The Real Veggies",context),
-          buildColumnWithRow(
-              "3", "My small farm",context),
-        ],
-      ),
-    );
-  }
-   GestureDetector buildColumnWithRow(String image, String title, BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                return DetailsPage(image:image,title:title,context:context);
-              }));
-      },
-        child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
-              height: 150,
-              width: 120,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: (Image.asset(
-                  "assets/images/blog$image.jpg",
-                  fit: BoxFit.cover,
-                )),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              title,
-              style: TextStyle(fontSize: 16),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class LineBar2 extends StatelessWidget {
-  const LineBar2({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 5,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          bottomLeft: Radius.circular(10),
-        ),
-      ),
-      margin: EdgeInsets.only(left: 25),
-      alignment: Alignment.centerLeft,
-      child: Container(
-        height: 5,
-        width: 100,
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class LineBar extends StatelessWidget {
-  const LineBar({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 5,
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.only(left: 25),
-      alignment: Alignment.centerLeft,
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          bottomLeft: Radius.circular(10),
-        ),
-      ),
-      child: Container(
-        height: 5,
-        width: 100,
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-          ),
-        ),
-      ),
-    );
   }
 }
