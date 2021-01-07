8c5e51616ddc0b2b605840227c12dc3ae291a0c8
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2021-01-08 03:33:47 +0530

    Update body.dart

--- lib/app/pages/cart/components/body.dart
+++ lib/app/pages/cart/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter_svg/svg.dart';
-import 'package:urban_roof/app/models/cart1.dart';
+//import 'package:urban_roof/app/models/cart1.dart';
+import 'package:urban_roof/app/pages/buy/single_item_view.dart';
 
 //import '../../size_config.dart';
 import 'cart_card.dart';
@@ -13,19 +14,29 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
+    
+
+    if(addedList.length == 0){
+      return Container(
+        alignment: Alignment.center,
+        child: Text('Oh! Your cart seems pretty light.',
+                    style: TextStyle(fontSize: 20.0),
+        ),
+      );
+    }
     return Padding(
       padding:
           EdgeInsets.symmetric(horizontal: 20),
       child: ListView.builder(
-        itemCount: cartList.length,
+        itemCount: addedList.length,
         itemBuilder: (context, index) => Padding(
           padding: EdgeInsets.symmetric(vertical: 10),
           child: Dismissible(
-            key: Key(cartList[index].product.id.toString()),
+            key: Key(addedList[index].product.id.toString()),
             direction: DismissDirection.endToStart,
             onDismissed: (direction) {
               setState(() {
-                cartList.removeAt(index);
+                addedList.removeAt(index);
               });
             },
             background: Container(
@@ -41,7 +52,7 @@ class _BodyState extends State<Body> {
                 ],
               ),
             ),
-            child: CartCard(cart: cartList[index]),
+            child: CartCard(cart: addedList[index]),
           ),
         ),
       ),
