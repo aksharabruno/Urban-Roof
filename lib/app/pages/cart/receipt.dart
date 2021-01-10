babe6a78eef8d622edb4ba1e41f63a91c8dc4e84
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2021-01-10 23:38:36 +0530

    Update receipt.dart

--- lib/app/pages/cart/receipt.dart
+++ lib/app/pages/cart/receipt.dart
@@ -26,7 +26,147 @@ class Receipt extends StatelessWidget {
       ),
       ),
       drawer: builddrawerelement(context),
-      bottomNavigationBar: Container(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+            Container(
+              alignment: Alignment.center,
+              child: Text('Note: Make a note of this receipt for the time of delivery',
+                      textAlign: TextAlign.center,),
+              
+            ),
+            SizedBox(height:20,),
+            Container(height: 20, color: Colors.green[100]),
+            SizedBox(height:20,),
+            Text('URBAN ROOF',
+                  style: TextStyle(fontSize: 50.0, ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width-135,
+              child: Container(height: 2, color: Colors.green),
+            ),
+            SizedBox(height:40,),
+            Container(
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              height: 40,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(border: Border.all(color: Colors.green)),
+              child: Text('Seller Details'),
+            ),
+            SizedBox(height:10,),
+            ]),
+            ListView.builder(
+              itemCount: addedList.length,
+              shrinkWrap: true,
+              itemBuilder: (context,index)=>SellerDetails(seller: addedList[index],)),
+            SizedBox(height: 20),
+            Container(height: 1, color: Colors.green),
+            SizedBox(height: 20),
+            Row(children: [
+              SizedBox(width:20),
+              Text('Total:', style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+              SizedBox(width: 60,),
+              Text('Rs. ${a.totalvalue()}', style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+            ],),
+            SizedBox(height: 20),
+            Container(height: 1, color: Colors.green),
+          ]
+        )
+      ),
+      bottomNavigationBar:  Container(
+        height: 55,
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[300],
+                    blurRadius: 20,
+                    spreadRadius: 1)
+              ]),
+          child: FlatButton(
+            onPressed: () {
+              int n = addedList.length;
+              addedList.removeRange(0, n);
+              Navigator.pushReplacementNamed(context, '/home');
+            }, 
+              child: Text('DONE',
+                style: TextStyle(color: Colors.white, fontSize: 30),  
+              )
+          ),
+        )),
+    );
+  }
+}
+
+class SellerDetails extends StatelessWidget {
+  const SellerDetails({
+    Key key,
+    @required this.seller,
+  }) : super(key: key);
+
+  final Added seller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 88,
+          child: AspectRatio(
+            aspectRatio: 0.88,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F6F9),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Image.asset(seller.product.images),
+            ),
+          ),
+        ),
+        SizedBox(width: 20),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              seller.product.title,
+              style: TextStyle(color: Colors.black, fontSize: 16),
+              maxLines: 2,
+            ),
+            
+            Text.rich(
+              TextSpan(
+                text: "Rs ${seller.product.price}",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600, color: kPrimaryColor),
+                children: [
+                  TextSpan(
+                      text: " x${seller.numOfItem}",
+                      style: Theme.of(context).textTheme.bodyText1),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Text('${seller.product.seller}'),
+            Text('${seller.product.address}...'),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+/*bottomNavigationBar: Container(
         height: 200.0,
       padding: EdgeInsets.symmetric(
         vertical: 15,//getProportionateScreenWidth(15),
@@ -103,114 +243,4 @@ class Receipt extends StatelessWidget {
           ],
         ),
       ),
-    ),
-   /* body: SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            padding: EdgeInsets.all(20.0),
-            //height: 60.0,
-            //color: Colors.grey[200],
-            decoration: BoxDecoration(border: Border.all(color: Colors.green[100], width: 5.0),),
-            child: 
-              Text(
-                'Note: Please take a screenshot of this receipt in case of future requirements.',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black54, fontSize: 20.0),
-              )
-            ),
-            
-          ListView.builder(
-            itemCount: addedList.length,
-            itemBuilder: (context, index) => Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-            child: SellerDetails(seller: addedList[index]),
-            )
-          ),
-            /*Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.all(20.0),
-              child: Column(
-                children:[
-                  Text('Seller Details', style: TextStyle(fontSize: 20.0),),
-                  SizedBox(height: 20.0),
-                  /*for (Added  p in addedList) {
-                    sellerdetails(p.product.title, p.product.type, p.product.seller, p.product.address, p.product.phone),
-                  },*/
-                  sellerdetails('tomato','vegetable','sample seller', '28/3, north street, \nmtm,','99999999999'),
-                  sellerdetails('cucumber','vegetable','sample seller', '28/3, north street, \nmtm,','99999999999'),
-                  sellerdetails('spinach','vegetable','sample seller', '28/3, north street, \nmtm,','99999999999'),
-                ]
-              )
-            ),*/
-            SizedBox(
-              height: 8.0,
-              child: Container(color: Colors.green[100],),
-            ),
-            
-          ]
-        ),
-      )*/
-    );
-  }
-}
-
-class SellerDetails extends StatelessWidget {
-  const SellerDetails({
-    Key key,
-    @required this.seller,
-  }) : super(key: key);
-
-  final Added seller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          width: 88,
-          child: AspectRatio(
-            aspectRatio: 0.88,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Color(0xFFF5F6F9),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Image.asset(seller.product.images),
-            ),
-          ),
-        ),
-        SizedBox(width: 20),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              seller.product.title,
-              style: TextStyle(color: Colors.black, fontSize: 16),
-              maxLines: 2,
-            ),
-            
-            Text.rich(
-              TextSpan(
-                text: "Rs ${seller.product.price}",
-                style: TextStyle(
-                    fontWeight: FontWeight.w600, color: kPrimaryColor),
-                children: [
-                  TextSpan(
-                      text: " x${seller.numOfItem}",
-                      style: Theme.of(context).textTheme.bodyText1),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Text('${seller.product.seller}'),
-            Text('${seller.product.address}...'),
-          ],
-        )
-      ],
-    );
-  }
-}
+    ),*/
