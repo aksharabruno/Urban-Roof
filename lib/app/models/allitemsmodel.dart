9c7f320fd69c6a42e2cb1c125aa13c82218b52d6
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2021-01-10 19:37:04 +0530

    Update allitemsmodel.dart

--- lib/app/models/allitemsmodel.dart
+++ lib/app/models/allitemsmodel.dart
@@ -2,19 +2,18 @@ import 'package:flutter/material.dart';
 //this is all item model
 class Item {
   final int id;
-  final String title, seller, address, qty;
-  final List<String> images;
-  final List<Color> colors;
-  final double rating, price;
+  final String title, seller, address, qty, type;
+  final String images, desc;
+  final double price;
   final int phone;
 
   Item({
     @required this.id,
+    @required this.type,
     @required this.images,
-    @required this.colors,
-    this.rating = 0.0,
     @required this.title,
     @required this.price,
+    @required this.desc,
     @required this.seller,
     @required this.address,
     @required this.phone,
@@ -24,85 +23,150 @@ class Item {
 
 // Our demo Items
 
-List<Item> demoItems = [
+List<Item> allItems = [
   Item(
     id: 1,
-    images: [
-      "assets/images/vegetabletomato.jpg",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
+    type: 'sapling',
+    images: "assets/images/saplingtomato.jpg",
     title: "Tomato",
     price: 50,
-    seller: seller,
-    rating: 4.8,
+    seller: 'abc nursery',
     phone: 9999999999,
     address: 'so n so n so n so address',
-    qty: '1 kg',
+    qty: '1 plant',
+    desc: 'fresh and organic',
   ),
   Item(
     id: 2,
-    images: [
-      "assets/images/vegetablecucumber.jpg",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Cucumber",
+    type: 'sapling',
+    images: "assets/images/saplingcabbage.jpg",
+    title: "Cabbage",
     price: 42,
-    seller: seller,
-    rating: 4.1,
+    seller: 'abc nursery',
     phone: 9999999999,
     address: 'so n so n so n so address',
-    qty: '0.5 kg',
+    qty: '1 plant',
+    desc: 'fresh and organic',
   ),
   Item(
     id: 3,
-    images: [
-      "assets/images/vegetablecarrot.jpg",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Carrot",
+    type: 'sapling',
+    images: "assets/images/saplingbrinjal.jpg",
+    title: "Brinjal",
     price: 30,
-    seller: seller,
-    rating: 4.1,
+    seller: 'abc nursery',
     phone: 9999999999,
     address: 'so n so n so n so address',
-    qty: '1 kg',
+    qty: '1 plant',
+    desc: 'fresh and organic',
   ),
   Item(
     id: 4,
-    images: [
-      "assets/images/vegetablespinach.jpg",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Spinach",
+    type: 'sapling',
+    images: "assets/images/saplingchilli.jpg",
+    title: "Chilli",
     price: 12,
-    seller: seller,
-    rating: 4.1,
+    seller: 'abc nursery',
     phone: 9999999999,
     address: 'so n so n so n so address',
-    qty: '0.25 kg'
+    qty: '1 plant',
+    desc: 'fresh and organic',
+  ),
+  Item(
+    id: 5,
+    type: 'vegetable',
+    images: "assets/images/vegetabletomato.jpg",
+    title: "Tomato",
+    price: 50,
+    seller: 'xyz vendor',
+    phone: 9999999999,
+    address: 'so n so n so n so address',
+    qty: '1 kg',
+    desc: 'fresh and organic',
+  ),
+  Item(
+    id: 6,
+    type: 'vegetable',
+    images: "assets/images/vegetablecucumber.jpg",
+    title: "Cucumber",
+    price: 42,
+    seller: 'xyz vendor',
+    phone: 9999999999,
+    address: 'so n so n so n so address',
+    qty: '0.5 kg',
+    desc: 'fresh and organic',
+  ),
+  Item(
+    id: 7,
+    type: 'vegetable',
+    images: "assets/images/vegetablecarrot.jpg",
+    title: "Carrot",
+    price: 30,
+    seller: 'xyz vendor',
+    phone: 9999999999,
+    address: 'so n so n so n so address',
+    qty: '1 kg',
+    desc: 'fresh and organic',
+  ),
+  Item(
+    id: 8,
+    type: 'seed',
+    images: "assets/images/seedspinach.jpg",
+    title: "Spinach",
+    price: 12,
+    seller: 'xyz vendor',
+    phone: 9999999999,
+    address: 'so n so n so n so address',
+    qty: '0.25 kg',
+    desc: 'fresh and organic',
+  ),
+  Item(
+    id: 9,
+    type: 'seed',
+    images: "assets/images/seedtomato.jpg",
+    title: "Tomato",
+    price: 50,
+    seller: 'xyz vendor',
+    phone: 9999999999,
+    address: 'so n so n so n so address',
+    qty: '1 packet(0.25 grams)',
+    desc: 'fresh and organic',
+  ),
+  Item(
+    id: 10,
+    type: 'seed',
+    images: "assets/images/seedladyfinger.jpg",
+    title: "Lady Finger",
+    price: 42,
+    seller: 'xyz vendor',
+    phone: 9999999999,
+    address: 'so n so n so n so address',
+    qty: '1 packet(0.25 grams)',
+    desc: 'fresh and organic',
+  ),
+  Item(
+    id: 11,
+    type: 'seed',
+    images: "assets/images/seedspinach.jpg",
+    title: "Spinach",
+    price: 30,
+    seller: 'xyz vendor',
+    phone: 9999999999,
+    address: 'so n so n so n so address',
+    qty: '1 packet(0.25 grams)',
+    desc: 'fresh and organic',
+  ),
+  Item(
+    id: 12,
+    type: 'seed',
+    images: "assets/images/seedlettuce.jpg",
+    title: "lettuce",
+    price: 12,
+    seller: 'xyz vendor',
+    phone: 9999999999,
+    address: 'so n so n so n so address',
+    qty: '1 packet(0.25 grams)',
+    desc: 'fresh and organic',
   ),
   
 ];
-
-const String seller =
-    "Veggies";
