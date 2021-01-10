6e64d6d90baca9fd0f8d191b0f4c75a8fae4a159
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2021-01-10 20:57:03 +0530

    Update todolist.dart

--- lib/app/pages/todolist.dart
+++ lib/app/pages/todolist.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:urban_roof/app/common/common_widgets.dart';
+//import 'package:flutter/src/rendering/object.dart';
 
-class Todo extends StatelessWidget {
+
+var newtask;
+List<String> tasks = [];
+
+final newtaskCon = new TextEditingController();
+class Todo extends StatefulWidget{
+  @override
+  _Todo createState() => _Todo();
+}
+class _Todo extends State<Todo> {
+  bool cond = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       drawer: builddrawerelement(context),
       bottomNavigationBar: Container(
+        
               height: 55,
               child: Container(
                 decoration: BoxDecoration(
@@ -25,8 +39,8 @@ class Todo extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     buildContainerBottomNav(Icons.ballot, 'allitems', context),
-                    buildContainerBottomNav(Icons.notifications_on_rounded, 'notification', context),
-                    buildContainerBottomNav(Icons.home, 'home', context, isSelected: true),
+                    //buildContainerBottomNav(Icons.notifications_on_rounded, 'notification', context),
+                    buildContainerBottomNav(Icons.home, 'home', context),
                     buildContainerBottomNav(Icons.person, 'profile', context),
                     buildContainerBottomNav(Icons.shopping_cart, 'maincategory', context),
                   ],
@@ -59,6 +73,7 @@ class Todo extends StatelessWidget {
                 overflow: Overflow.visible,
                 children: <Widget>[
                   Container(
+                    padding: EdgeInsets.all(20),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
@@ -66,28 +81,36 @@ class Todo extends StatelessWidget {
                           topLeft: Radius.circular(40)),
                     ),
                     child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(
-                            "Task No $index",
-                            style: TextStyle(
-                                color: Colors.grey[900],
-                                fontWeight: FontWeight.bold),
+                      itemCount: tasks.length,
+                      itemBuilder: (context, index) => Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Dismissible(
+                          key: Key(tasks[index]),
+                          direction: DismissDirection.endToStart,
+                          onDismissed: (direction) {
+                            setState(() {
+                              tasks.removeAt(index);
+                            });
+                          },
+                          background: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFFFFE6E6),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Row(
+                              children: [
+                                Spacer(),
+                                Icon(Icons.delete_outline),
+                                SizedBox(width:20)
+                              ],
+                            ),
                           ),
-                          subtitle: Text(
-                            "This is the detail of task No $index",
-                            style: TextStyle(color: Colors.grey[700]),
-                          ),
-                          trailing: Icon(
-                            Icons.check_circle,
-                            color: Colors.greenAccent,
-                          ),
-                          isThreeLine: true,
-                        );
-                      },
-                      controller: scrolController,
-                      itemCount: 20,
-                    ),
+                          
+                          child: TaskCard(taskitem: tasks[index]),
+                        ),
+                        ),
+                        controller: scrolController,
+                      )
                   ),
                   Positioned(
                     child: FloatingActionButton(
@@ -96,7 +119,10 @@ class Todo extends StatelessWidget {
                         color: Colors.white,
                       ),
                       backgroundColor: Colors.black,
-                      onPressed: () {},
+                      onPressed: () {
+                        print('hi');
+                        _openPopup(context);
+                      },
                     ),
                     top: -30,
                     right: 30,
@@ -111,3 +137,74 @@ class Todo extends StatelessWidget {
   }
 }
 
+_openPopup(context) {
+  String newtask;
+  final newtaskCon = new TextEditingController();
+    Alert(
+        context: context,
+        title: "New Task",
+        content: Column(
+          children: <Widget>[
+            TextField(
+              controller: newtaskCon,
+              decoration: InputDecoration(
+                icon: Icon(Icons.edit_location_rounded),
+              ),
+            ),
+          ],
+        ),
+        buttons: [
+          DialogButton(
+            onPressed: () {
+              newtask = newtaskCon.text;
+              tasks.addAll({newtask});
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              "Add",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          )
+        ]).show();
+  }
+
+class TaskCard extends StatelessWidget{
+  const TaskCard({
+    Key key,
+    @required this.taskitem,
+  }): super(key: key);
+
+  final taskitem;
+  
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      children: [
+        SizedBox(
+          width: 88,
+          child: AspectRatio(
+            aspectRatio: 0.88,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F6F9),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Image.asset('assets/icons/app_icon.png'),
+            ),
+          ),
+        ),
+        SizedBox(width: 20),
+        Expanded(child: Text(
+              taskitem,
+              style: TextStyle(color: Colors.black, fontSize: 18),
+              maxLines: 2,
+            ),),
+            
+            //SizedBox(height: 10),
+          ],
+        
+      
+    );
+  }
+}
