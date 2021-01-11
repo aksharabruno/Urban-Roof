bb8889470ae545aca85b0d6574024fa342993c43
Saranya2110 <70891898+Saranya2110@users.noreply.github.com>
2021-01-11 09:23:23 +0530

    Update home.dart

--- lib/app/pages/home.dart
+++ lib/app/pages/home.dart
@@ -1,187 +1,174 @@
 //import 'dart:ui';
 
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:urban_roof/app/common/common_widgets.dart';
-
+import 'grow.dart';
 import 'detailspage.dart';
 
 class HomePage extends StatelessWidget {
-  //final String uname;
-  //HomePage({Key key, @required this.uname}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          drawer: builddrawerelement(context),
-          bottomNavigationBar: Container(
-              height: 55,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey[300],
-                          blurRadius: 20,
-                          spreadRadius: 1)
-                    ]),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    buildContainerBottomNav(Icons.ballot, 'allitems', context),
-                    //buildContainerBottomNav(Icons.notifications_on, 'notification', context),
-                    buildContainerBottomNav(Icons.home, 'home', context, isSelected: true),
-                    buildContainerBottomNav(Icons.person, 'profile', context),
-                    buildContainerBottomNav(Icons.shopping_cart, 'maincategory', context),
-                  ],
+      
+      drawer: builddrawerelement(context),
+      bottomNavigationBar: Container(
+          height: 55,
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
                 ),
-              )),
-          appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.green,
-            elevation: 0,
-            leading: Builder(builder: (BuildContext context) {
-              return IconButton(
-                icon: Container(child: Icon(Icons.menu)),
-                onPressed: () {},
-              );
-            }),
-            title: Text(
-              "URBAN ROOF",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey[300], blurRadius: 20, spreadRadius: 1)
+                ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildContainerBottomNav(Icons.ballot, 'allitems', context),
+                buildContainerBottomNav(
+                    Icons.notifications_on_rounded, 'notification', context),
+                buildContainerBottomNav(Icons.home, 'home', context,
+                    isSelected: true),
+                buildContainerBottomNav(Icons.person, 'profile', context),
+                buildContainerBottomNav(
+                    Icons.shopping_cart, 'maincategory', context),
+              ],
             ),
-            actions: <Widget>[
-              IconButton(
-                icon: Container(child: Icon(Icons.search)),
-                onPressed: () {},
-              ),
-            ],
+          )),
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          elevation: 0,
+          
+          title: Text(
+            "URBAN ROOF",
           ),
-          body: Column(
+          actions: <Widget>[
+            IconButton(
+              icon: Container(child: Icon(Icons.search)),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 8,
+          ),
+          Padding(padding: new EdgeInsets.all(5)),
+          Row(
             children: [
               SizedBox(
-                height: 8,
+                width: 10,
               ),
-              Padding(padding: new EdgeInsets.all(5)),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Padding(padding: new EdgeInsets.all(2)),
-                  Text("Start your Farm",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 0.5,
-                      )),
-                ],
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  buildFlatButton("Blogs", context, 'home', isSelected: true),
-                  buildFlatButton("Grow", context, 'grow'),
-                  buildFlatButton("Reviews", context, 'home'),
-                ],
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius:
-                          BorderRadius.only(topLeft: Radius.circular(40)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey[350],
-                          spreadRadius: 1,
-                          blurRadius: 15,
-                        )
-                      ]),
-                  child: SingleChildScrollView(
-                    physics: BouncingScrollPhysics(),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            buildColumnAtTop("Top", isSelected: true),
-                            buildColumnAtTop("Popular"),
-                            buildColumnAtTop("Trending"),
-                            buildColumnAtTop("Editor's choice"),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        MyblogList(),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        LineBar(),
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(
-                            children: [
-                              Text(
-                                "Beginner Special",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                ),
-                              ),
-                              Spacer(),
-                              Text(
-                                "View All",
-                                style:
-                                    TextStyle(fontSize: 15, color: Colors.grey),
-                              )
-                            ],
-                          ),
-                        ),
-                        SingleChildScrollView(
-                          physics: BouncingScrollPhysics(),
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            children: [
-                              buildBottomContainer("4",
-                                  "Easy Chilli", "Author"),
-                              buildBottomContainer("5",
-                                  "Very green peas", "Author"),
-                              buildBottomContainer("6",
-                                  "Organic eggplants", "Author"),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        LineBar2(),
-                      ],
-                    ),
-                  ),
-                ),
-              )
+              Padding(padding: new EdgeInsets.all(2)),
+              Text("Start your Farm",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5,
+                  )),
             ],
           ),
-        );
+          SizedBox(
+            height: 13,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildFlatButton(context, "Blogs", isSelected: true),
+              buildFlatButton(context, "Grow"),
+              buildFlatButton(context, "Reviews"),
+            ],
+          ),
+          SizedBox(
+            height: 13,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[350],
+                      spreadRadius: 1,
+                      blurRadius: 15,
+                    )
+                  ]),
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildColumnAtTop("Top", isSelected: true),
+                        buildColumnAtTop("Popular"),
+                        buildColumnAtTop("Trending"),
+                        buildColumnAtTop("Editor's choice"),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    MyblogList(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LineBar(),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Beginner Special",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          Spacer(),
+                          Text(
+                            "View All",
+                            style: TextStyle(fontSize: 15, color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          buildBottomContainer("4", "Easy Chilli", "Author"),
+                          buildBottomContainer(
+                              "5", "Very green peas", "Author"),
+                          buildBottomContainer(
+                              "6", "Organic eggplants", "Author"),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LineBar2(),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
-
-
 
   Container buildBottomContainer(String image, String text1, String text2) {
     return Container(
@@ -195,7 +182,8 @@ class HomePage extends StatelessWidget {
                 child: SizedBox(
                     height: 130,
                     width: 100,
-                    child: Image.asset("assets/images/blog$image.jpg", fit: BoxFit.cover))),
+                    child: Image.asset("assets/images/blog$image.jpg",
+                        fit: BoxFit.cover))),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
@@ -222,8 +210,6 @@ class HomePage extends StatelessWidget {
         ));
   }
 
- 
-
   Column buildColumnAtTop(String text, {bool isSelected = false}) {
     return Column(
       children: [
@@ -247,10 +233,18 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  FlatButton buildFlatButton(String text,  BuildContext context, String route, {bool isSelected = false}) {
+  FlatButton buildFlatButton(context, text, {bool isSelected = false}) {
     return FlatButton(
       onPressed: () {
-        Navigator.pushReplacementNamed(context, '/$route');
+        if (text == 'Blogs') {
+          Navigator.of(context).pushNamed('/home');
+        }
+        if (text == 'Grow') {
+          Navigator.of(context).pushNamed('/grow');
+        }
+        if (text == 'Reviews') {
+          Navigator.of(context).pushNamed('/home');
+        }
       },
       child: Text(
         text,
@@ -281,28 +275,23 @@ class MyblogList extends StatelessWidget {
           SizedBox(
             width: 20,
           ),
-          
-            buildColumnWithRow(
-                "1",
-                "Fresh from Home",context),
-          
-          buildColumnWithRow("2",
-              "The Real Veggies",context),
-          buildColumnWithRow(
-              "3", "My small farm",context),
+          buildColumnWithRow("1", "Fresh from Home", context),
+          buildColumnWithRow("2", "The Real Veggies", context),
+          buildColumnWithRow("3", "My small farm", context),
         ],
       ),
     );
   }
-   GestureDetector buildColumnWithRow(String image, String title, BuildContext context) {
+
+  GestureDetector buildColumnWithRow(
+      String image, String title, BuildContext context) {
     return GestureDetector(
-      onTap: (){
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                return DetailsPage(image:image,title:title,context:context);
-              }));
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return DetailsPage(image: image, title: title, context: context);
+        }));
       },
-        child: Padding(
+      child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
