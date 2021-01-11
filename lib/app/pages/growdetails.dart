21b9162ee9c7c6488e55f944c1a95bd00c155f8f
Akshara Bruno <52251990+aksharabruno@users.noreply.github.com>
2021-01-11 11:06:01 +0530

    Update growdetails.dart

--- lib/app/pages/growdetails.dart
+++ lib/app/pages/growdetails.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:urban_roof/app/common/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
 class GrowDetailsPage extends StatelessWidget {
   final String image;
   final String title;
   final BuildContext context;
-  GrowDetailsPage({this.image, this.title, this.context});
+  final String link;
+  final String text;
+  GrowDetailsPage({this.image, this.title, this.context, this.link, this.text});
   @override
+  
   Widget build(BuildContext context) {
+    
     return Scaffold(
+      
         body: Stack(
-       
+          
+         alignment: Alignment.topCenter,
       children: [
         
         Image.asset("assets/images/crop$image.JPG",
-            width: MediaQuery.of(context).size.width,
+         
+            width: MediaQuery.of(context).size.width/2,
+            fit: BoxFit.cover,
             height: MediaQuery.of(context).size.height /3),
-       IconButton(
+        IconButton(
           padding: EdgeInsets.only(left: 10, top: 30),
           icon: Icon(
             Icons.close,
-            color: Colors.black,
+            color: Colors.white,
           ),
           onPressed: () {
             Navigator.of(context).pop();
@@ -28,6 +37,7 @@ class GrowDetailsPage extends StatelessWidget {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
+             
             height: MediaQuery.of(context).size.height / 2.6,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
@@ -38,16 +48,37 @@ class GrowDetailsPage extends StatelessWidget {
               ),
             ),
             child: Column(
+              
               children: [
                 Expanded(
-                    child: Column(
-                  children: [],
-                )),
+                  child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                     
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    
+                    SizedBox(height: 20),
+                    Text('What you need to know to grow $title', style: TextStyle(fontSize: 30),),
+                    SizedBox(height: 30),
+                    Expanded(child: Text(text, style: TextStyle(fontSize: 20),))
+                  
+
+                  ],
+                ))),
                 Container(
                     height: 60,
                     width: MediaQuery.of(context).size.width,
                     color: Colors.green,
-                    child: Row(
+                    child: FlatButton(
+                      onPressed: () {
+                        if(canLaunch(link) != null){
+                           launch(link);
+                        } else {
+                          throw 'Could not load page';
+                        }
+                      },
+                      child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Text(
@@ -59,11 +90,15 @@ class GrowDetailsPage extends StatelessWidget {
                         )
                       ],
                     ))
+                  )
               ],
             ),
           ),
         ),
       ],
+       
     ));
+    
   }
 }
+
